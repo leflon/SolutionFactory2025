239fb83aa7d616dcb30e80fe05f88d7714b2215b
Thomas MARCHAL <marchalthomaswork@gmail.com>
2025-06-24 17:42:18 +0200

    [ADD] PMR access

--- src/app/api/autocomplete/route.ts
+++ src/app/api/autocomplete/route.ts
@@ -3,20 +3,35 @@ import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
-  //Reduce the query to lowercase for case-insensitive search
-  // and ensure it is not empty
   const q = searchParams.get('q')?.toLowerCase() || '';
 
   if (!q) {
     return Response.json({ error: 'Missing query parameter "q"' }, { status: 400 });
   }
 
-  // Use parameterized LIKE query for prefix matching, limit to 10 results
-  // Make sure to have distinct results to avoid duplicates
-  const stmt = db.prepare(
-    'SELECT DISTINCT name FROM Stops WHERE LOWER(name) LIKE ? ORDER BY name LIMIT 10'
-  );
-  const stations = stmt.all(`${q}%`);
+  // Grab the stations from the database
+  // GROUP_CONCAT(stop_id) as stop_ids, 
+  const stmt = db.prepare(`
+    SELECT 
+      name, 
+      MAX(wheelchair_accessible) as wheelchair_accessible
+    FROM Stops
+    WHERE LOWER(name) LIKE ?
+    GROUP BY name
+    ORDER BY name
+    LIMIT 10
+  `);
+
+  const rows = stmt.all(`${q}%`);
+
+  // Formating the result 
+  const stations = rows.map(row => {
+    const r = row as { name: string; wheelchair_accessible: number };
+    return {
+      name: r.name,
+      wheelchair_accessible: !!r.wheelchair_accessible
+    };
+  });
 
   return Response.json({ stations });
 }
