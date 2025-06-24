f75376e6d8c056020f914f050d8eba5253df0ba4
Louis Hislaire <122865223+GitAzuXo@users.noreply.github.com>
2025-06-24 14:24:37 +0200

    [ADD] Navbar (Languages and Dark mode) + Entry fields

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
-		<html lang='fr'>
+		<html lang='fr' className="bg-gray-150 dark:bg-gray-800">
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
