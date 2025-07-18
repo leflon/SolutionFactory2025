8713824f474a568bde0ac95d83bc4ea58815f3d4
Paul Leflon <leflon.paul@gmail.com>
2025-07-18 12:00:56 +0200

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,59 @@
 import { LanguageProvider } from '@/components/LanguageProvider';
 import '@/styles/main.css';
-import type { Metadata } from 'next';
-import Chatbot from '@/components/Chatbot';
 
-export const metadata: Metadata = {
-	title: 'Metro Efrei Dodo',
+export const metadata = {
+	title: 'Roule Ma Poule',
 	description: 'Votre assistant mobilité intelligent',
+	icons: {
+		apple: '/icons/apple-icon.png',
+		shortcut: '/icons/favicon.ico',
+		other: [
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '96x96',
+				url: '/icons/favicon.png'
+			},
+			{
+				rel: 'icon',
+				type: 'image/svg+xml',
+				url: '/icons/favicon.svg'
+			}
+		]
+	},
+	applicationName: 'Roule Ma Poule',
+	openGraph: {
+		title: 'Roule Ma Poule | Votre assistant mobilité intelligent',
+		description:
+			'Découvrez Roule Ma Poule, l’assistant intelligent qui simplifie vos trajets dans les transports en commun parisiens.',
+		url: 'https://rmp.leflon.fr',
+		siteName: 'Roule Ma Poule',
+		images: [
+			{
+				url: 'https://rmp.leflon.frog-image.jpg', // Make sure this image is at least 1200x630 pixels
+				width: 1200,
+				height: 630,
+				alt: 'Roule Ma Poule'
+			}
+		],
+		locale: 'fr_FR',
+		type: 'website'
+	},
+	manifest: '/manifest.json',
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Roule Ma Poule | Votre assistant mobilité intelligent',
+		description:
+			'Découvrez Roule Ma Poule, l’assistant intelligent qui simplifie vos trajets dans les transports en commun parisiens.',
+		images: ['https://rmp.leflon.fr/og-image.jpg']
+	},
+	other: {
+		'apple-mobile-web-app-title': 'Roule Ma Poule'
+	}
 };
 
 export default function RootLayout({
-	children,
+	children
 }: {
 	children: React.ReactNode;
 }) {
